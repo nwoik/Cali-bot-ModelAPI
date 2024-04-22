dc242271637fbe19f80e717b26b914fad2e44419
nwoik <71194216+nwoik@users.noreply.github.com>
2024-04-22 20:37:19 +0100

    cleaned up code. Removed members

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 
 func main() {
 	clans := c.NewClans().Open("./clan.json")
-	members := c.NewMembers().Open("members.json")
+	members := utils.Open("members.json")
 	// member := utils.CreateMember("12345", "nikka", "deez", "123131414")
 
 	// clans.Add(*member)
 	clan := utils.AddClan(clans, "My Clan", "129712342", "123131")
 	clan.SetMembers(utils.AddMember(clan.GetMembers(), "12345", "nikka", "deez", "123131414"))
-	members.SetMembers(utils.AddMember(members.GetMembers(), "12345", "nikka", "deez", "123131414"))
+	members = utils.AddMember(members, "12345", "nikka", "deez", "123131414")
 
 	clans.Close("./clan2.json")
-	members.Close("./members.json")
+	utils.Close("./members.json", members)
 }
--- utils/modelAPI.go
+++ utils/modelAPI.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	clans "github.com/nwoik/calibotapi/clans"
 )
 
@@ -34,4 +38,56 @@ func CreateMember(nick string, ign string, igid string, userid string) clans.Mem
 		SetUserID(userid)
 
 	return *newMember
+}
+
+func Open(filePath string) []*clans.Member {
+	var members []*clans.Member
+	jsonData, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading JSON file:", err)
+		return nil
+	}
+
+	// Parse JSON data
+	err = json.Unmarshal(jsonData, &members)
+	if err != nil {
+		fmt.Println("Error parsing JSON data:", err)
+		return nil
+	}
+
+	return members
+}
+
+func Close(filePath string, members []*clans.Member) {
+
+	// Open the JSON file for reading
+	file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("Error opening JSON file:", err)
+		return
+	}
+	defer file.Close()
+
+	// // Rewind the file pointer to the beginning
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		fmt.Println("Error rewinding file pointer:", err)
+		return
+	}
+
+	// // Truncate the file to remove any existing content
+	err = file.Truncate(0)
+	if err != nil {
+		fmt.Println("Error truncating file:", err)
+		return
+	}
+
+	// // Encode and write updated JSON data back to the file
+	err = json.NewEncoder(file).Encode(&members)
+	if err != nil {
+		fmt.Println("Error encoding JSON data:", err)
+		return
+	}
+
+	fmt.Println("Data has been written to", filePath)
 }
